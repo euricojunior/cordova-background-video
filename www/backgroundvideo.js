72d11a80d2b3c1ad91d43cf67dbd2d885866eb0b
euricojunior <euricojr@gmail.com>
2023-09-25 21:52:50 -0300

    Update backgroundvideo.js

--- www/backgroundvideo.js
+++ www/backgroundvideo.js
@@ -11,6 +11,9 @@ var backgroundvideo = {
     },
     stop : function(successFunction, errorFunction) {
         cordova.exec(successFunction, errorFunction, 'backgroundvideo','stop', []);
+    },
+	getprofiles : function(successFunction, errorFunction) {
+        cordova.exec(successFunction, errorFunction, 'backgroundvideo','getprofiles', []);
     }
 };
 
